2e9a47b21fe9a601bdd7a074958301081813226c
root <root@sea-dev-mms-pkgtest-05.amer.gettywan.com>
2017-06-27 02:23:33 -0700

    arsf

--- buzz/generator.py
+++ buzz/generator.py
@@ -17,9 +17,8 @@ def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
-    return phrase.title()
+    return "le %s" % (phrase.title())
 
 if __name__ == "__main__":
-    print 'second edition!'
     print generate_buzz()
 
