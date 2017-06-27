9e4170a8baa40c76f29ec4e6b87467b6b390bc39
root <root@sea-dev-mms-pkgtest-05.amer.gettywan.com>
2017-06-27 02:07:30 -0700

    arsf

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo apt-get install -y docker-engine
-
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
             TAG="latest"
     else
                 TAG="$TRAVIS_BRANCH"
         fi
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
